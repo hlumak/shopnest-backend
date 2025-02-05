d024bd5b3fb5699aa75a65d06cafde73ff7abee3
hlumak <vglumakov@gmail.com>
2025-02-05 19:31:05 +0200

    Add review entity and implement colors logic

---
+++ server/src/color/dto/color.dto.ts
@@ -0,0 +1,9 @@
+import { IsString } from 'class-validator';
+
+export class ColorDto {
+  @IsString({ message: 'Name is required' })
+  name: string;
+
+  @IsString({ message: 'Value is required' })
+  value: string;
+}
