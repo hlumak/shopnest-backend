c940d88eb0b1ee08b71f5b6f0d173f66feb8bf97
hlumak <vglumakov@gmail.com>
2025-02-05 21:55:48 +0200

    Implement category logic

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,7 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { CategoryDto } from './dto/category.dto';
 
-@Controller('category')
+@Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Auth()
+  @Get('stores/:storeId')
+  async getManyByStoreId(@Param('storeId') storeId: string) {
+    return this.categoryService.getManyByStoreId(storeId);
+  }
+
+  @Auth()
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.categoryService.getById(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Post(':storeId')
+  async create(@Param('storeId') storeId: string, @Body() dto: CategoryDto) {
+    return this.categoryService.create(storeId, dto);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: CategoryDto) {
+    return this.categoryService.update(id, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.categoryService.delete(id);
+  }
 }
--- server/src/category/category.service.ts
+++ server/src/category/category.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CategoryDto } from './dto/category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async getManyByStoreId(storeId: string) {
+    return this.prisma.category.findMany({
+      where: {
+        storeId
+      }
+    });
+  }
+
+  async getById(id: string) {
+    const color = await this.prisma.category.findUnique({
+      where: { id }
+    });
+
+    if (!color) throw new NotFoundException('Category not found');
+
+    return color;
+  }
+
+  async create(storeId: string, dto: CategoryDto) {
+    return this.prisma.category.create({
+      data: {
+        ...dto,
+        storeId
+      }
+    });
+  }
+
+  async update(id: string, dto: CategoryDto) {
+    await this.getById(id);
+
+    return this.prisma.category.update({
+      where: { id },
+      data: dto
+    });
+  }
+
+  async delete(id: string) {
+    await this.getById(id);
+
+    return this.prisma.category.delete({
+      where: { id }
+    });
+  }
+}
