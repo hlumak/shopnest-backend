74e8cf64ed3ce3460807c31b0b817728edc7aba6
hlumak <vglumakov@gmail.com>
2025-02-11 19:26:17 +0200

    Add uploads

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { StoreController } from './store.controller';
-import {PrismaService} from '../prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [StoreController],
-  providers: [StoreService, PrismaService],
+  providers: [StoreService, PrismaService]
 })
 export class StoreModule {}
