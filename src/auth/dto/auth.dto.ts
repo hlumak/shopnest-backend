02340df591aa705ce77a4a8aabf7aa9a6c69c3d4
hlumak <vglumakov@gmail.com>
2025-05-26 01:31:50 +0300

    update dependencies and auth validation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,15 +1,25 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength
+} from 'class-validator';
 
 export class AuthDto {
   @IsOptional()
+  @MinLength(2, { message: 'Name must contain more than 2 characters!' })
+  @MaxLength(35, { message: 'Name must contain less than 35 characters!' })
   @IsString()
   name: string;
 
   @IsString({ message: 'Email is required' })
+  @MaxLength(50, { message: 'Email must contain less than 50 characters!' })
   @IsEmail()
   email: string;
 
   @MinLength(6, { message: 'Password must contain more than 6 characters!' })
+  @MaxLength(20, { message: 'Password must contain less than 20 characters!' })
   @IsString({ message: 'Password is required' })
   password: string;
 }
